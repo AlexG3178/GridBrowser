80abbe4652cf4c954b8e86cb07f41610233bb0b6
alexg3178 <alexg3178@gmail.com>
2019-02-18 20:33:57 +0200

    Initial Commit

---
+++ GridBrowser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GridBrowser
+//
+//  Created by Alexandr Grigoriev on 2/18/19.
+//  Copyright © 2019 AlexG. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GridBrowser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GridBrowser
+//
+//  Created by Alexandr Grigoriev on 2/18/19.
+//  Copyright © 2019 AlexG. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
